8c1bfe08f3dcb761b145156d1ab5c9d65ad9d40a
manvith <iib2019014@iiita.ac.in>
2022-02-03 17:24:42 +0530

    1) added id's to each nav-item 2) added a identity-div for each template 3) set sidenav-item-active to that nav-item present with identity-div

--- js/index.js
+++ js/index.js
@@ -24,3 +24,19 @@ toggler.addEventListener("click", function(event) {
     }
 
 })
+
+// sidenav_items = document.querySelectorAll(".sidenav-item");
+
+// sidenav_items.forEach(function (item) {
+//     item.addEventListener("click", function (event) {
+//         active_item = document.querySelector(".sidenav-item-active");
+//         console.log(active_item);
+//         active_item.classList.remove("sidenav-item-active");
+//         item.classList.add("sidenav-item-active");
+//     })
+// })
+
+active_page_util = document.querySelector(".keep-link-active");
+active_page = active_page_util.classList[1]
+active_nav_item = document.getElementById(active_page)
+active_nav_item.classList.add("sidenav-item-active")
